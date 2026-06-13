cddb9fdf0b1141dad2471da6065b27b03e8c2176
terumina198-bit <terumina198@gmail.com>
2026-06-13 11:18:02 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -17,24 +17,24 @@ export const civIcons = {
 };
 
 export const rows = [
-  ["軍事","陸戦","military_land"],
-  ["軍事","海戦","military_sea"],
+  ["軍事","陸戦(赤)","military_land"],
+  ["軍事","海戦(青)","military_sea"],
 
-  ["経済","コイン","economy_coin"],
-  ["経済","借財","economy_debt"],
-  ["経済","商業","economy_trade"],
+  ["経済","コイン(金額)","economy_coin"],
+  ["経済","借財(マイナス入力)","economy_debt"],
+  ["経済","商業(黄)","economy_trade"],
 
-  ["建設","ワンダー","build_wonder"],
-  ["建設","ポイント","build_point"],
-  ["建設","ギルド","build_guild"],
-  ["建設","シティ","build_city"],
+  ["建設","ワンダー(文明)","build_wonder"],
+  ["建設","ポイント(青)","build_point"],
+  ["建設","ギルド(紫)","build_guild"],
+  ["建設","シティ(黒)","build_city"],
 
-  ["拡張","リーダー","expansion_leader"],
+  ["拡張","リーダー(白)","expansion_leader"],
   ["拡張","海戦ボード","expansion_naval"],
   ["拡張","ヤシの木","expansion_palm"],
 
-  ["科学","歯車","science_gear"],
-  ["科学","コンパス","science_compass"],
-  ["科学","石板","science_tablet"],
-  ["科学","ALL","science_all"]
+  ["科学","歯車(緑)","science_gear"],
+  ["科学","コンパス(緑)","science_compass"],
+  ["科学","石板(緑)","science_tablet"],
+  ["科学","ALL(緑)","science_all"]
 ];
